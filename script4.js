ec9b41ca6cfb3192ea2a36aaed2b5c99b5750675
RISHABH MISHRA <92362688+R1SH4BH81@users.noreply.github.com>
2023-06-29 12:19:02 +0530

    Update script4.js

--- script4.js
+++ script4.js
@@ -22,3 +22,38 @@ function theme() {
     themeIcon.title = 'Switch to dark mode';
   }
 }
+document.addEventListener('keydown', function(event) {
+  // Check if Alt + W is pressed
+  if (event.altKey && event.key === 'w') {
+    changeWallpaper();
+  }
+});
+
+
+let wallpaperUrls = [
+  'https://images.unsplash.com/photo-1566738780863-f9608f88f3a9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2378&q=80',
+  'https://th.bing.com/th/id/OIP.7HRtOe-L_ymXQZ5mXZ18nQHaEK?pid=ImgDet&rs=1',
+  'https://wallpaperaccess.com/full/4408994.jpg',
+  'https://images.wallpapersden.com/image/download/kite-colorful-pattern_bGZoZmmUmZqaraWkpJRmbmdlrWZnZWU.jpg',
+  'https://th.bing.com/th/id/R.71da97160b8a15de4f769ef5c946b924?rik=v9mPbR4W3ieoWw&riu=http%3a%2f%2fwallup.net%2fwp-content%2fuploads%2f2016%2f01%2f311977-abstract.jpg&ehk=6tUQa3h%2bBwrTI89RIXdsbOryEGqRprr5P2CTWeHOlfI%3d&risl=&pid=ImgRaw&r=0'
+  // Add more wallpaper URLs as needed
+];
+let currentWallpaperIndex = 0;
+
+function changeWallpaper() {
+  var desktop = document.querySelector('.desktop-1');
+  
+  currentWallpaperIndex = (currentWallpaperIndex + 1) % wallpaperUrls.length;
+  var newWallpaper = wallpaperUrls[currentWallpaperIndex];
+
+  desktop.style.backgroundImage = `url("${newWallpaper}")`;
+}
+
+
+
+document.addEventListener('keydown', function(event) {
+  // Check if Alt + T is pressed
+  if (event.altKey && event.key === 'q') {
+    theme();
+  }
+});
