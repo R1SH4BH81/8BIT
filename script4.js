57ec256d59b05686b3fedf305a8f78f01198088b
RISHABH MISHRA <92362688+R1SH4BH81@users.noreply.github.com>
2023-07-01 12:57:45 +0530

    Update script4.js

--- script4.js
+++ script4.js
@@ -1,12 +1,12 @@
-window.addEventListener('load', function() {
-  var screenWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-  var isMobileDevice = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
-  var isTabletDevice = /Tablet|iPad|Android/i.test(navigator.userAgent); // Check for tablet device
+window.addEventListener('load', function () {
+            var screenWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+            var isMobileDevice = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
 
-  if (screenWidth < 768 || isMobileDevice || !isTabletDevice) {
-    window.location.href = 'restricted.html';
-  }
-});
+            if (screenWidth < 960 || isMobileDevice) {
+                window.location.href = 'restricted.html';
+            }
+        });
+
 
 function theme() {
   const body = document.body;
