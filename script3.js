63fe1b51b5e56b85965b0f13f9c1e01a170b6c64
RISHABH MISHRA <92362688+R1SH4BH81@users.noreply.github.com>
2023-07-01 19:24:05 +0530

    Update script3.js

--- script3.js
+++ script3.js
@@ -31,4 +31,27 @@ document.addEventListener('keydown', function(event) {
     }
   });
   
+function isNightTime() {
+    const now = new Date();
+    const hours = now.getHours();
+    const minutes = now.getMinutes();
+    const currentTime = hours * 60 + minutes; // Convert current time to minutes
+
+    // Convert 6 PM IST to minutes (18 hours * 60 minutes)
+    const nightTime = 18 * 60;
+
+    return currentTime >= nightTime;
+}
+
+// Function to toggle dark mode
+function toggleDarkMode() {
+    const body = document.body;
+    body.classList.toggle("dark-mode");
+}
+
+// Check time on page load
+if (isNightTime()) {
+    toggleDarkMode();
+}
+
   
