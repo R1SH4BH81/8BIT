d45b50a139ff5807827281d11a2853265934dfa5
RISHABH MISHRA <92362688+R1SH4BH81@users.noreply.github.com>
2023-07-02 08:53:11 +0530

    Update session.js

--- session.js
+++ session.js
@@ -10,37 +10,46 @@ sweetAlertStylesheet.href = 'https://cdn.jsdelivr.net/npm/sweetalert2@11.0.19/di
 document.head.appendChild(sweetAlertStylesheet);
 
 function checkSessionExpiration() {
-    var sessionTimeout = 30 * 60 * 1000; // 30 minutes in milliseconds
-    var lastActivity = parseInt(localStorage.getItem('lastActivity'));
-    var currentTime = Date.now();
+  var sessionTimeout = 30 * 60 * 1000; // 30 minutes in milliseconds
+  var lastActivity = parseInt(localStorage.getItem('lastActivity'));
+  var currentTime = Date.now();
 
-    if (currentTime - lastActivity > sessionTimeout) {
-      // Session has expired
-      // Display SweetAlert notification
+  if (currentTime - lastActivity > sessionTimeout) {
+    // Session has expired
+    if (!localStorage.getItem('sessionExpired')) {
+      // Display SweetAlert notification only if it hasn't been shown before
       Swal.fire({
         icon: 'error',
         title: 'Session Expired',
         text: 'Your session has expired. Please log in again.',
-      }).then(function() {
-        // Perform any necessary actions after the user closes the notification
-        // For example, you can redirect the user to a login page:
-        window.location.href = 'login.html';
+        showCancelButton: false,
+        showConfirmButton: true,
+        confirmButtonText: 'OK'
+      }).then(function(result) {
+        // Check if the user clicked the "OK" button
+        if (result.isConfirmed) {
+          // Set sessionExpired flag in local storage to indicate the notification has been shown
+          localStorage.setItem('sessionExpired', 'true');
+          // Redirect the user to the login page
+          window.location.href = 'login.html';
+        }
       });
-    } else {
-      // Session is still active
-      // Update the last activity timestamp
-      localStorage.setItem('lastActivity', currentTime.toString());
     }
+  } else {
+    // Session is still active
+    // Update the last activity timestamp
+    localStorage.setItem('lastActivity', currentTime.toString());
+  }
+}
+
+// Call the checkSessionExpiration function when necessary
+// For example, you can call it on page load or at regular intervals using a timer
+window.addEventListener('load', function() {
+  // Set initial last activity timestamp if not already set
+  if (!localStorage.getItem('lastActivity')) {
+    localStorage.setItem('lastActivity', Date.now().toString());
   }
 
-  // Call the checkSessionExpiration function when necessary
-  // For example, you can call it on page load or at regular intervals using a timer
-  window.addEventListener('load', function() {
-    // Set initial last activity timestamp if not already set
-    if (!localStorage.getItem('lastActivity')) {
-      localStorage.setItem('lastActivity', Date.now().toString());
-    }
-
-    // Call the checkSessionExpiration function periodically
-    setInterval(checkSessionExpiration, 1000); // Adjust the interval as needed
-  });
+  // Call the checkSessionExpiration function periodically
+  setInterval(checkSessionExpiration, 1000); // Adjust the interval as needed
+});
